a2de37e24364765bbdcf362e17815d12986a5d58
RajiAujla <rajwinderaujla061@gmail.com>
2024-04-18 21:48:44 -0400

    use of APIs with Nodejs and Expressjs

---
+++ 5.6 Secrets Project/index.js
@@ -0,0 +1,31 @@
+// HINTS:
+// 1. Import express and axios
+import express from 'express';
+import axios from 'axios';
+import bodyParser from "body-parser";
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+// 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+// 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+    try{
+    const response = await axios.get("https://api.quotable.io/random");
+    const data = response.data;
+    console.log(data);
+    res.render("index.ejs",{secret:data.content,
+    user: data.author});
+    }catch(error){
+      res.status(500).send(error.response.data);
+    }
+  });
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
